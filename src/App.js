402525ae44ab64e7bba95d0434987abb87661f1f
Karan Kewat <84654445+karankewat02@users.noreply.github.com>
2022-05-03 00:16:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/De7YmfcKFbMNGl-M/scene.spline" />
+    </div>
+  );
+}
+
+
